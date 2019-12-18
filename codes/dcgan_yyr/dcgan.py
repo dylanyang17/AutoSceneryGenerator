224dd05c0629dadd21fd169ff5439272044ea6e9
yyr <315629555@qq.com>
2019-12-19 04:53:35 +0800

    [add] 完善keras版本的dcgan, 正在尝试运行

--- codes/dcgan_yyr/dcgan.py
+++ codes/dcgan_yyr/dcgan.py
@@ -1,6 +1,20 @@
 import numpy as np
+import os
+import datetime
 import matplotlib.pyplot as plt
 import keras
+
+log_path = './log.txt'
+train_dir = './train'
+
+
+def debug(s):
+    # 输出字符串, 并且追加打印到log.txt中
+    timestamp = datetime.datetime.now().strftime('%m-%d %H:%M:%S:: ')
+    print(timestamp + s)
+    with open(file=log_path, mode='a') as f:
+        print(timestamp + s, file=f)
+
 
 class DCGAN():
     def __init__(self):
@@ -25,7 +39,10 @@ class DCGAN():
         self.combined.compile(loss=self.loss_func, optimizer=keras.optimizers.Adam(lr=self.lr))
 
     def build_discriminator(self):
-        # 构建 discriminator
+        """
+        构建 Discriminator，注意传入值为 -1~1 的图片
+        :return:
+        """
         model = keras.models.Sequential()
         model.add(keras.layers.Conv2D(input_shape=self.img_shape, filters=64, kernel_size=4, strides=2, padding='same'))
         model.add(keras.layers.BatchNormalization(momentum=self.bn_momentum))
@@ -51,6 +68,10 @@ class DCGAN():
         return keras.models.Model(img, score)
 
     def build_generator(self):
+        """
+        构建 Generator，注意生成值为 -1~1 的图片
+        :return:
+        """
         model = keras.models.Sequential()
         model.add(keras.layers.Dense(512*8*8, input_shape=self.noise_shape))
         model.add(keras.layers.Reshape((8, 8, 512)))
@@ -75,10 +96,123 @@ class DCGAN():
         img = model(z)
         return keras.models.Model(z, img)
 
-    def train(self, epochs, batch_size, save_interval):
-        np.load(self.data_path)
+    def train(self, start_epoch, end_epoch, batch_size, save_interval):
+        """
+        进行训练，训练的起始轮数为 start_epoch，终止轮数为 end_epoch（闭区间），
+        每一轮选出 batch_size 张图片，每隔 save_interval 轮进行一次保存
+        """
+        # data 的形状: (number of images, 128, 128, 3)，注意将其变换到 [-1, 1] 上
+        data = np.load(self.data_path)
+        data = data / 127.5 - 1
+        real = np.ones(shape=(batch_size, 1))
+        fake = np.zeros(shape=(batch_size, 1))
+        d_loss_list = []
+        g_loss_list = []
+        for epoch in range(start_epoch + 1, end_epoch + 1):
+            start_time = datetime.datetime.now()
+            # Train Discriminator
+            idx = np.random.randint(0, data.shape[0], batch_size)
+            real_imgs = data[idx]
+            z = np.random.normal(0, 1, size=(batch_size,)+self.noise_shape)
+            fake_imgs = self.generator.predict(z)
+            d_loss_real = self.discriminator.train_on_batch(real_imgs, real)
+            d_loss_fake = self.discriminator.train_on_batch(fake_imgs, fake)
+            d_loss = 0.5 * np.add(d_loss_fake, d_loss_real)
+            d_loss_list.append(d_loss)
+
+            # Train Generator
+            g_loss = self.combined.train_on_batch(z, real)
+            g_loss_list.append(g_loss)
+            time_cost = (datetime.datetime.now()-start_time).total_seconds()
+            debug('epoch: %d  time_cost: %d [D loss: %f  acc: %f] [G loss: %f]' %
+                  (epoch, time_cost, d_loss[0], d_loss[1], g_loss))
+
+            # Save
+            if epoch % save_interval == 0:
+                self.save_model(epoch)
+                self.save_imgs(epoch)
+
+    def save_imgs(self, epoch):
+        """
+        保存第 epoch 轮生成的图片
+        :param epoch: 要保存信息的轮数
+        :return:
+        """
+        debug('save images, epoch: %d' % epoch)
+        save_dir = os.path.join(train_dir, 'epoch' + str(epoch))
+        os.makedirs(save_dir, exist_ok=True)
+
+        r, c = 5, 5
+        noise = np.random.normal(0, 1, (r * c,)+self.noise_shape)
+        fake_imgs = self.generator.predict(noise)
+
+        # 变换到 [0, 255] 上
+        fake_imgs = int(np.round((fake_imgs + 1) / 2 * 255))
+
+        fig, axs = plt.subplots(r, c)
+        cnt = 0
+        for i in range(r):
+            for j in range(c):
+                axs[i, j].imshow(fake_imgs[cnt, :, :, :])
+                axs[i, j].axis('off')
+                cnt += 1
+        fig.savefig(os.path.join(save_dir, 'img.png'))
+        plt.close()
+
+
+    def save_model(self, epoch):
+        """
+        保存第 epoch 轮的信息
+        :param epoch: 要保存信息的轮数
+        :return:
+        """
+        # 保存模型
+        debug('save model, epoch: %d' % epoch)
+        save_dir = os.path.join(train_dir, 'epoch' + str(epoch))
+        os.makedirs(save_dir, exist_ok=True)
+
+        self.combined.save(os.path.join(save_dir, 'combined'))
+        self.generator.save(os.path.join(save_dir, 'generator'))
+        self.discriminator.save(os.path.join(save_dir, 'discriminator'))
+
+    def load_model(self, epoch):
+        """
+        载入第 epoch 轮保存的信息
+        :param epoch: 保存信息的轮数
+        :return:
+        """
+        debug('load model, epoch: %d' % epoch)
+        save_dir = os.path.join(train_dir, 'epoch' + str(epoch))
+        self.combined = keras.models.load_model(os.path.join(save_dir, 'combined'))
+        self.generator = keras.models.load_model(os.path.join(save_dir, 'generator'))
+        self.discriminator = keras.models.load_model(os.path.join(save_dir, 'discriminator'))
+
+
+def filter_save_dir(s):
+    """
+    filter函数, 筛选出以epoch开头的目录, 均为保存数据的目录
+    """
+    return s.startswith('epoch')
+
+
+def get_last_epoch():
+    """
+    获得最新保存的训练的 epoch
+    :return: int 类型的 epoch，若不存在则返回 -1
+    """
+    dir_list = os.listdir(train_dir)
+    dir_list = list(filter(filter_save_dir, dir_list))
+    max_epoch = -1
+    for name in dir_list:
+        epoch = int(name[5:])
+        max_epoch = max(epoch, max_epoch)
+    return max_epoch
+
 
 if __name__ == '__main__':
     dcgan = DCGAN()
-
-    # dcgan.train()
+    os.makedirs(train_dir, exist_ok=True)
+    start_epoch = get_last_epoch()
+    if start_epoch != -1:
+        dcgan.load_model(start_epoch)
+    dcgan.train(start_epoch=start_epoch, end_epoch=10, batch_size=32, save_interval=5)
