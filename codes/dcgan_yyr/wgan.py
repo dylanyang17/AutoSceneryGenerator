a4342487bc66c1dea57b599ec00ac6dbc8bf4e76
yyr <315629555@qq.com>
2019-12-21 05:04:40 +0800

    [add] 完善wgan代码

--- codes/dcgan_yyr/wgan.py
+++ codes/dcgan_yyr/wgan.py
@@ -18,19 +18,20 @@ def debug(s):
 
 class WGAN():
     def __init__(self):
-        self.d_lr = 0.0002  # learning rate for discriminator
-        self.g_lr = 0.0003   # learning rate for generator
         self.bn_momentum = 0.95
         self.channels = 3
         self.img_shape = (64, 64, self.channels)
         self.noise_shape = (100,)
-        self.loss_func = 'binary_crossentropy'
+
+        self.d_train_times = 5
+        self.clip_value = 0.01
+        self.optimizer = keras.optimizers.RMSprop(lr=0.00005)
         self.data_path = '../../data/mountains64.npy'
 
         self.base_discriminator = self.build_discriminator()
         self.generator = self.build_generator()
         self.discriminator = keras.models.Model(self.base_discriminator.inputs, self.base_discriminator.outputs)
-        self.discriminator.compile(loss=self.loss_func, optimizer=keras.optimizers.Adam(lr=self.d_lr),
+        self.discriminator.compile(loss=self.wasserstein_loss, optimizer=self.optimizer,
                                    metrics=['accuracy'])
         self.discriminator_frozen = keras.models.Model(self.base_discriminator.inputs, self.base_discriminator.outputs)
         self.discriminator_frozen.trainable = False
@@ -39,8 +40,11 @@ class WGAN():
         img = self.generator(z)
         score = self.discriminator_frozen(img)
         self.combined = keras.models.Model(z, score)
-        self.combined.compile(loss=self.loss_func, optimizer=keras.optimizers.Adam(lr=self.g_lr),
+        self.combined.compile(loss=self.wasserstein_loss, optimizer=self.optimizer,
                                 metrics=['accuracy'])
+
+    def wasserstein_loss(self, y_true, y_pred):
+        return keras.backend.mean(y_true * y_pred)
 
     def build_discriminator(self):
         """
@@ -69,7 +73,7 @@ class WGAN():
         model.add(keras.layers.LeakyReLU())
         model.add(keras.layers.Dropout(0.25))
         model.add(keras.layers.Flatten())
-        model.add(keras.layers.Dense(1, activation='sigmoid'))
+        model.add(keras.layers.Dense(1))  # 不加Activation
 
         img = keras.layers.Input(shape=self.img_shape)
         score = model(img)
@@ -83,19 +87,18 @@ class WGAN():
         model = keras.models.Sequential()
         model.add(keras.layers.Dense(1024*4*4, input_shape=self.noise_shape))
         model.add(keras.layers.Reshape((4, 4, 1024)))
+        model.add(keras.layers.Deconv2D(filters=1024, kernel_size=5, strides=2, padding='same'))
+        model.add(keras.layers.BatchNormalization(momentum=self.bn_momentum))
+        model.add(keras.layers.Activation('relu'))
         model.add(keras.layers.Deconv2D(filters=512, kernel_size=5, strides=2, padding='same'))
         model.add(keras.layers.BatchNormalization(momentum=self.bn_momentum))
         model.add(keras.layers.Activation('relu'))
         model.add(keras.layers.Deconv2D(filters=256, kernel_size=5, strides=2, padding='same'))
         model.add(keras.layers.BatchNormalization(momentum=self.bn_momentum))
         model.add(keras.layers.Activation('relu'))
-        model.add(keras.layers.Deconv2D(filters=128, kernel_size=5, strides=2, padding='same'))
-        model.add(keras.layers.BatchNormalization(momentum=self.bn_momentum))
-        model.add(keras.layers.Activation('relu'))
-        # model.add(keras.layers.Deconv2D(filters=64, kernel_size=5, strides=2, padding='same'))
+        # model.add(keras.layers.Deconv2D(filters=128, kernel_size=5, strides=2, padding='same'))
         # model.add(keras.layers.BatchNormalization(momentum=self.bn_momentum))
         model.add(keras.layers.Deconv2D(filters=self.channels, kernel_size=5, strides=2, padding='same'))
-# model.add(keras.layers.BatchNormalization(momentum=self.bn_momentum))
         model.add(keras.layers.Activation('tanh'))
         model.add(keras.layers.Reshape(self.img_shape))
 
@@ -112,46 +115,36 @@ class WGAN():
         # data 的形状: (number of images, 256, 256, 3)，注意将其变换到 [-1, 1] 上
         data = np.load(self.data_path)
         data = data / 127.5 - 1
-        real = np.ones(shape=(batch_size, 1))
-        fake = np.zeros(shape=(batch_size, 1))
+        real = -np.ones(shape=(batch_size, 1))
+        fake = np.ones(shape=(batch_size, 1))
         d_loss_list = []
         g_loss_list = []
-        g_train_times = 1
         for epoch in range(start_epoch + 1, end_epoch + 1):
             start_time = datetime.datetime.now()
             debug('training on epoch ' + str(epoch))
-            if g_train_times >= 7:
-                pass
-            else:
-                for i in range(d_train_times):
-                    # Train Discriminator
-                    idx = np.random.randint(0, data.shape[0], batch_size)
-                    real_imgs = data[idx] + np.random.normal(0, 0.001, self.img_shape)  # 加入噪声
-                    z = np.random.normal(0, 1, size=(batch_size,)+self.noise_shape)
-                    fake_imgs = self.generator.predict(z)
-                    d_loss_real = self.discriminator.train_on_batch(real_imgs, real)
-                    d_loss_fake = self.discriminator.train_on_batch(fake_imgs, fake)
-                    d_loss = 0.5 * np.add(d_loss_fake, d_loss_real)
-                    debug('d_train iteration: %d  d_loss: %f  d_acc: %f' % (i, d_loss[0], d_loss[1]))
-                    d_loss_list.append(d_loss)
-            # Dynamically adjust
-            if d_loss[1] < 0.6:
-                d_train_times += 1
-            else:
-                d_train_times = max(d_train_times//2, 1)
-                
-            for i in range(g_train_times):
-                # Train Generator
-                z = np.random.normal(0, 1, size=(batch_size,) + self.noise_shape)
-                g_loss = self.combined.train_on_batch(z, real)
-                g_loss_list.append(g_loss)
-                debug('g_train iteration: %d  g_loss: %f  g_acc: %f]' %
-                  (i, g_loss[0], g_loss[1]))
-            # Dynamically adjust
-            if g_loss[1] < 0.15:
-                g_train_times += 1
-            else:
-                g_train_times = max(g_train_times//2, 1)
+            for i in range(self.d_train_times):
+                # Train Discriminator
+                idx = np.random.randint(0, data.shape[0], batch_size)
+                real_imgs = data[idx] + np.random.normal(0, 0.0001, self.img_shape)  # 加入噪声
+                z = np.random.normal(0, 1, size=(batch_size,)+self.noise_shape)
+                fake_imgs = self.generator.predict(z)
+                d_loss_real = self.discriminator.train_on_batch(real_imgs, real)
+                d_loss_fake = self.discriminator.train_on_batch(fake_imgs, fake)
+                d_loss = 0.5 * np.add(d_loss_fake, d_loss_real)
+                debug('D:: d_train iteration: %d  d_loss: %f  d_acc: %f' % (i, d_loss[0], d_loss[1]))
+                d_loss_list.append(d_loss)
+
+                # Clip
+                for l in self.discriminator.layers:
+                    weights = l.get_weights()
+                    weights = [np.clip(w, -self.clip_value, self.clip_value) for w in weights]
+                    l.set_weights(weights)
+
+            # Train Generator
+            g_loss = self.combined.train_on_batch(z, real)
+            g_loss_list.append(g_loss)
+            debug('G:: g_loss: %f  g_acc: %f]' %
+              (g_loss[0], g_loss[1]))
 
             time_cost = (datetime.datetime.now()-start_time).total_seconds()
             debug('finish epoch %d  time_cost: %d' % (epoch, time_cost))
@@ -216,7 +209,7 @@ class WGAN():
         self.generator = keras.models.load_model(os.path.join(save_dir, 'generator'))
         self.base_discriminator = keras.models.load_model(os.path.join(save_dir, 'base_discriminator'))
         self.discriminator = keras.models.Model(self.base_discriminator.inputs, self.base_discriminator.outputs)
-        self.discriminator.compile(loss=self.loss_func, optimizer=keras.optimizers.Adam(lr=self.d_lr),
+        self.discriminator.compile(loss=self.wasserstein_loss, optimizer=self.optimizer,
                                    metrics=['accuracy'])
         self.discriminator_frozen = keras.models.Model(self.base_discriminator.inputs, self.base_discriminator.outputs)
         self.discriminator_frozen.trainable = False
@@ -225,7 +218,7 @@ class WGAN():
         img = self.generator(z)
         score = self.discriminator_frozen(img)
         self.combined = keras.models.Model(z, score)
-        self.combined.compile(loss=self.loss_func, optimizer=keras.optimizers.Adam(lr=self.g_lr),
+        self.combined.compile(loss=self.wasserstein_loss, optimizer=self.optimizer,
                               metrics=['accuracy'])
 
 def filter_save_dir(s):
